ecd70194a6664e68b8e1b8b6d3ea335c941832bc
Marco Catarino <marco.catarino60@gmail.com>
2025-07-14 10:01:52 -0500

    feat: add log in controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -105,20 +105,43 @@ export const verifyEmailController = async (req, res) => {
 
 // todo: Log In Controller
 export const loginController = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    const user = req.body;
-    console.log(user);
+    const user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged In Successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
   } catch (error) {
-    return res.status(400).json({ error: "Error in: " });
+    console.log("Error in Log In", error);
+    return res.status(400).json({ success: false, message: "Error in Log In" });
   }
 };
 
 // todo: Log Out Controller
 export const logoutController = async (req, res) => {
-  try {
-    const user = req.body;
-    console.log(user);
-  } catch (error) {
-    return res.status(400).json({ error: "Error in: " });
-  }
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged Out Successfully" });
 };
