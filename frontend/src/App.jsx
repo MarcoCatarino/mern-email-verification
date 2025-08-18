61fb8d8cba01cb948eca5d30bfcdeba9e6f9573c
Marco Catarino <marco.catarino60@gmail.com>
2025-08-18 01:14:23 -0500

    feat: add ideas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Background } from "./components/ui/Background";
 import { SignUp } from "./pages/SignUp";
 import { LogIn } from "./pages/LogIn";
 import { Home } from "./pages/Home";
+import { VerifyEmail } from "./pages/VerifyEmail";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/log-in" element={<LogIn />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
         </Routes>
       </BrowserRouter>
     </div>
